2fe824cbd98ec8cc7942266de4974d552c9b3147
llucasreis <pro.lucasreis@gmail.com>
2021-09-03 11:09:42 -0300

    feat: create middlewares for gin
    - logger
    - basicAuth
    - dump

--- server.go
+++ server.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/llucasreis/go-gin/controller"
+	"github.com/llucasreis/go-gin/middlewares"
 	"github.com/llucasreis/go-gin/service"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -11,8 +16,18 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+	server := gin.New()
+
+	setupLogOutput()
+
+	server.Use(gin.Recovery(), middlewares.Logger(),
+		middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(c *gin.Context) {
 		c.JSON(200, videoController.FindAll())
